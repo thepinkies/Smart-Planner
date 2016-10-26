ba908965132513744990e6664c7640ff85eabbae
hiisolde <ifmalfaro@gmail.com>
2016-10-26 00:35:40 -0700

    Isolde test

---
+++ src/test/java/edu/csupomona/cs480/IsoldeTest.java
@@ -0,0 +1,24 @@
+package edu.csupomona.cs480;
+
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+
+public class IsoldeTest {
+	@Test
+	public void stringTest() {
+		System.out.print("Null string test result: ");
+		assertNull(createNullString());
+		System.out.println("Valid string test result: ");
+		assertNull(createString());
+	}
+
+	public String createNullString() {
+		String test = null;
+		return test;
+	}
+
+	public String createString() {
+		String test2 = "Hello World!";
+		return test2;
+	}
+}

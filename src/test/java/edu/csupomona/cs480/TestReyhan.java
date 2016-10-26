43a43dd41360e1668ddd4d5c2b717150b27cda9f
rfpatia <rfpatia@cpp.edu>
2016-10-25 20:13:12 -0700

    Reyhan Testing

---
+++ src/test/java/edu/csupomona/cs480/TestReyhan.java
@@ -0,0 +1,26 @@
+package edu.csupomona.cs480;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+ 
+public class TestReyhan {
+ 
+	@Test
+	public void Testingaddition() {
+		assertEquals("Here is test for Addition Result: ", 30, addition(27, 3));
+	}
+ 
+	@Test
+	public void testingHelloWorld() {
+		assertEquals("Here is test for Hello World String: ", "Hello + World", helloWorld());
+	}
+ 
+	public int addition(int x, int y) {
+		return x + y;
+	}
+ 
+	public String helloWorld() {
+		String helloWorld = "Hello +" + " World";
+		return helloWorld;
+	}
+}
